20b318d814d1ade15708cc7bc61f5a4910ac8de4
Ben P <ben.h.poh@gmail.com>
2020-04-29 17:40:04 +1000

    Batches pages partially completed

--- academy/main.rb
+++ academy/main.rb
@@ -51,87 +51,162 @@ get "/main/students" do
   batch_number = nil
 
   user = list_staff_by_id(session["user_id"])
-  all_students = list_all_students
+  # all_students = list_all_students
+  # unassigned_students = list_unassigned_students
   active_students = list_active_students
-  unassigned_students = list_unassigned_students
   active_batches = list_active_batches(active_students)
   batch_students = list_students_by_batch_number(batch_number)
-  student = list_student_by_id(params["id"])
+  sel_student = list_student_by_id(params["id"])
 
 
   erb :students_show, locals: {
     user: user, 
-    all_students: all_students,
-    active_students: active_students,
-    batch_students: batch_students,
-    unassigned_students: unassigned_students,
+    batch_number: batch_number,
+    # all_students: all_students,
+    # active_students: active_students,
+    # unassigned_students: unassigned_students,
     active_batches: active_batches,
-    student: student
+    batch_students: batch_students,
+    sel_student: sel_student
+  }
+end
+
+get "/main/students/all" do
+  redirect "/" unless logged_in?
+  batch_number = nil
+
+  user = list_staff_by_id(session["user_id"])
+  active_students = list_active_students
+  active_batches = list_active_batches(active_students)
+  sel_student = list_student_by_id(params["id"])
+
+
+  erb :students_show, locals: {
+    user: user, 
+    batch_number: batch_number,
+    active_batches: active_batches,
+    batch_students: active_students,
+    sel_student: sel_student
+  }
+end
+
+get "/main/students/alumni" do
+  redirect "/" unless logged_in?
+  batch_number = nil
+
+  user = list_staff_by_id(session["user_id"])
+  active_students = list_active_students
+  active_batches = list_active_batches(active_students)
+  batch_students = list_graduated_students
+  sel_student = list_student_by_id(params["id"])
+
+
+  erb :students_show, locals: {
+    user: user, 
+    batch_number: batch_number,
+    active_batches: active_batches,
+    batch_students: batch_students,
+    sel_student: sel_student
   }
 end
 
 get "/main/students/:batch" do
   redirect "/" unless logged_in?
-  batch_number = params["batch"][1..-1]
+  batch_number = params["batch"][6..-1]
 
   user = list_staff_by_id(session["user_id"])
-  all_students = list_all_students
   active_students = list_active_students
-  unassigned_students = list_unassigned_students
   active_batches = list_active_batches(active_students)
   batch_students = list_students_by_batch_number(batch_number)
-  student = list_student_by_id(params["id"])
+  sel_student = list_student_by_id(params["id"])
 
   erb :students_show, locals: {
     user: user, 
-    all_students: all_students,
-    active_students: active_students,
-    batch_students: batch_students,
-    unassigned_students: unassigned_students,
+    batch_number: batch_number,
     active_batches: active_batches,
-    student: student
+    batch_students: batch_students,
+    sel_student: sel_student
   }
 end
 
 get "/main/students/:batch/:id" do
   redirect "/" unless logged_in?
-  batch_number = params["batch"][1..-1]
+  batch_number = params["batch"][6..-1]
 
   user = list_staff_by_id(session["user_id"])
-  all_students = list_all_students
   active_students = list_active_students
-  unassigned_students = list_unassigned_students
   active_batches = list_active_batches(active_students)
   batch_students = list_students_by_batch_number(batch_number)
-  student = list_student_by_id(params["id"])
+  sel_student = list_student_by_id(params["id"])
 
   erb :students_show, locals: {
     user: user, 
-    all_students: all_students,
-    active_students: active_students,
-    batch_students: batch_students,
-    unassigned_students: unassigned_students,
+    batch_number: batch_number,
     active_batches: active_batches,
-    student: student
+    batch_students: batch_students,
+    sel_student: sel_student
   }
+end
+
+patch "/main/students/graduate" do
+  graduate_student_by_id(params["id"])
+  redirect "/main/students/alumni"
 end
 
 get "/main/batches" do
   redirect "/" unless logged_in?
   user = list_staff_by_id(session["user_id"])
   active_students = list_active_students
-  unassigned_students = list_unassigned_students
-  all_students = list_all_students
-
   active_batches = list_active_batches(active_students)
+  unassigned_students = list_unassigned_students
 
   erb :batches_show, locals: {
     user: user, 
-    all_students: all_students,
-    active_students: active_students,
+    active_batches: active_batches,
     unassigned_students: unassigned_students,
-    active_batches: active_batches
+    batch: nil
   }
 end
 
+get "/main/batches/:batch" do
+  redirect "/" unless logged_in?
+  user = list_staff_by_id(session["user_id"])
+  active_students = list_active_students
+  active_batches = list_active_batches(active_students)
+  unassigned_students = list_unassigned_students
+  batch_students = list_students_by_batch_number(params["batch"])
+
+  erb :batches_show, locals: {
+    user: user, 
+    active_batches: active_batches,
+    unassigned_students: unassigned_students,
+    batch_students: batch_students,
+    batch: params["batch"],
+    sel_student: nil
+  }
+end
+
+get "/main/batches/:batch/:id" do
+  redirect "/" unless logged_in?
+  user = list_staff_by_id(session["user_id"])
+  active_students = list_active_students
+  active_batches = list_active_batches(active_students)
+  unassigned_students = list_unassigned_students
+  batch_students = list_students_by_batch_number(params["batch"])
+  sel_student = list_student_by_id(params["id"])
+
+  erb :batches_show, locals: {
+    user: user, 
+    active_batches: active_batches,
+    unassigned_students: unassigned_students,
+    batch_students: batch_students,
+    batch: params["batch"],
+    sel_student: sel_student
+  }
+end
+
+patch "/main/batches/assign" do
+  update_student_batch_number_by_id(params["id"], params["batch_number"])
+  redirect "/main/students/batch-#{params["batch_number"]}/#{params["id"]}"
+end
 
--- academy/model/students.rb
+++ academy/model/students.rb
@@ -21,8 +21,20 @@ def list_unassigned_students()
     run_sql("SELECT * FROM students WHERE batch_number IS NULL ORDER BY name;", [])
 end
 
+def list_graduated_students()
+    run_sql("SELECT * FROM students WHERE graduated IS true ORDER BY name;", [])
+end
+
+def list_active_batches(students)
+    active_batches = []
+    students.each do |student|
+        active_batches << student["batch_number"].to_i
+    end
+    active_batches.sort.uniq!
+end
+
 def list_students_by_batch_number(batch_number)
-    records = run_sql("SELECT * FROM students WHERE batch_number = $1;", [batch_number])
+    records = run_sql("SELECT * FROM students WHERE batch_number = $1 ORDER BY name;", [batch_number])
     if records.count == 0
         return nil
     else
@@ -49,10 +61,18 @@ def create_student()
     run_sql("INSERT INTO students (name, email, mobile, image_url, password_digested) VALUES ($1, $2, $3, $4, $5);", [name, student["email"], student["cell"], student["picture"]["large"], password_digested])
 end
 
-def update_student_by_id(id, name, email, mobile, image_url, password)
+def update_student_details_by_id(id, name, email, mobile, image_url, password)
     password_digested = BCrypt::Password.create(password)
 
     run_sql("UPDATE students SET name = $1, email = $2, mobile = $3, image_url = $4, password_digested = $5 WHERE id = $6;",[name, email, mobile, image_url, password_digested, id])
+end
+
+def update_student_batch_number_by_id(id, batch_number)
+    run_sql("UPDATE students SET batch_number = $1 WHERE id = $2;",[batch_number, id])
+end
+
+def graduate_student_by_id(id)
+    run_sql("UPDATE students SET graduated = true WHERE id = $1;", [id])
 end
 
 def delete_student_by_id(id)
